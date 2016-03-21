41bc9fb552b8d0835b76fa5d15cabfe905d161fb
Alex J. Malozemoff <amaloz@cs.umd.edu>
2016-03-21 15:44:32 -0400

    added comments

--- src/garble.h
+++ src/garble.h
@@ -11,12 +11,17 @@
 #define GARBLE_OK 0
 #define GARBLE_ERR -1
 
+/* Supported garbling types */
 typedef enum {
+    /* GRR3 and free-XOR as used in JustGarble */
     GARBLE_TYPE_STANDARD,
+    /* Half-gates approach of Zahur, Rosulek, and Evans (Eurocrypt 2015) */
     GARBLE_TYPE_HALFGATES,
+    /* Privacy-free approach of Zahur, Rosulek, and Evans (Eurocrypt 2015) */
     GARBLE_TYPE_PRIVACY_FREE,
 } garble_type_e;
 
+/* Supported gate types */
 typedef enum {
     GARBLE_GATE_ZERO = 0,
     GARBLE_GATE_ONE = 15,
@@ -32,9 +37,9 @@ typedef struct {
 } garble_wire;
 
 typedef struct {
-    /* the type of gate this is */
+    /* The type of gate this is */
     garble_gate_type_e type;
-    /* the input/output wires associated with this gate */
+    /* The input/output wires associated with this gate */
     uint64_t input0, input1, output;
 } garble_gate;
 
@@ -49,17 +54,24 @@ typedef struct {
 } garble_fixed_wire;
 
 typedef struct {
+    /* n: number of inputs */
+    /* m: number of outputs */
+    /* q: number of gates */
+    /* r: number of wires */
+    /* n_fixed_wires: number of fixed wires */
     uint64_t n, m, q, r, n_fixed_wires;
+    /* garbling scheme type */
     garble_type_e type;
     garble_gate *gates;         /* q */
     block *table;               /* q */
     garble_wire *wires;         /* r */
-    garble_fixed_wire *fixed_wires; /* <= r */
+    garble_fixed_wire *fixed_wires; /* n_fixed_wires */
     int *outputs;                   /* m */
     block fixed_label;
     block global_key;
 } garble_circuit;
 
+/* Return the table size of a garbled circuit */
 inline
 size_t garble_table_size(const garble_circuit *gc)
 {
@@ -74,36 +86,51 @@ size_t garble_table_size(const garble_circuit *gc)
     return 0;
 }
 
+/* Context info for building a circuit description */
 typedef struct {
     uint64_t wire_index;
     uint64_t n_fixed_wires;
     uint64_t n_gates;
 } garble_context;
 
+/* Create a new circuit */
 int
 garble_new(garble_circuit *gc, uint64_t n, uint64_t m, garble_type_e type);
+/* Delete a garbled circuit */
 void
 garble_delete(garble_circuit *gc);
 
+/* Call this before constructing a new circuit */
 void
 garble_start_building(garble_circuit *gc, garble_context *ctxt);
+/* Call this after completing the construction of a circuit */
 void
 garble_finish_building(garble_circuit *gc, garble_context *ctxt,
                        const int *outputs);
 
+/* Garbles a circuit.
+   If inputs is NULL, generate input-wire labels.
+   If outputs is NULL, don't store output-wire labels into outputs.
+ */
 int
 garble_garble(garble_circuit *gc, const block *inputs, block *outputs);
-
+/* Hash a given garbled circuit */
 void
 garble_hash(const garble_circuit *gc, unsigned char hash[SHA_DIGEST_LENGTH]);
+/* Check that 'gc' matches the hash specified in 'hash' */
 int
 garble_check(garble_circuit *gc, const unsigned char hash[SHA_DIGEST_LENGTH]);
+/* Create a random delta block */
 block
 garble_create_delta(void);
+/* Create 'n' input labels and store them in 'labels'.
+   'delta' specified the delta block to use; if NULL, generate a new delta.
+   'privacyfree' specifies whether you are using the privacy free garbling scheme.
+ */
 void
 garble_create_input_labels(block *labels, uint64_t n, block *delta,
                            bool privacyfree);
-
+/* Evaluate garbled circuit 'gc'. */
 int
 garble_eval(const garble_circuit *gc, const block *inputs, block *outputs);
 void
